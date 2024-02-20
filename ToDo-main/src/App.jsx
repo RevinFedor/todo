9a57e3b34f75893bdcf03725360412ef7ba6d74e
RevinFedor <51700693+RevinFedor@users.noreply.github.com>
2024-02-20 14:21:29 +0500

    Add files via upload

---
+++ ToDo-main/src/App.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [array, setArray] = useState([])
+  const [task, setTask] = useState('')
+
+  useEffect(() => {
+    const taskList = JSON.parse(localStorage.getItem('list'))
+    if (taskList) {
+      setArray(taskList)
+    }
+  }, [])
+
+  const setList = (task) => {
+    if (task) {
+      setArray([...array, { task: task, complete: false }])
+      localStorage.setItem('list', JSON.stringify([...array, { task: task, complete: false }]))
+    }
+  }
+
+  const updateList = (index, value) => {
+    const updateArray = array.map((item, i) =>
+      i == index ? { ...item, task: value } : item
+    )
+    setArray(updateArray);
+    localStorage.setItem('list', JSON.stringify(updateArray))
+  }
+
+  const complete = (index) => {
+    const updateArray = array.map((item, i) =>
+      i == index ? { ...item, complete: true } : item
+    )
+    setArray(updateArray)
+    localStorage.setItem('list', JSON.stringify(updateArray))
+  }
+
+  const deleteTask = (i) => {
+    const newArray = array.filter((a, index) => {
+      return index !== i
+    })
+    console.log(newArray);
+    setArray(newArray)
+    localStorage.setItem('list', JSON.stringify(newArray))
+  }
+
+  return (
+    <>
+    <div className="container">
+      <div className="task_input">
+        <input
+          type="text"
+          placeholder='Тут писать задачу:'
+          value={task}
+          onChange={(a) => setTask(a.target.value)} />
+        <button
+          onClick={() => {
+            setList(task);
+            setTask('')
+          }}
+          className='add_btn'>создать</button>
+      </div>
+      <div className="list">
+        {
+          array.map((e, index) => (
+            <div className="container_task" key={index}>
+              <input className={"task " + (e.complete ? "line" : "text")}
+                onChange={(a) => updateList(index, a.target.value)}
+                value={e.task} />
+              <div className="btns">
+                <button
+                  onClick={() => complete(index)}
+                  className='btn_yes'>сделано</button>
+                <button
+                  onClick={() => deleteTask(index)}
+                  className='btn_no'>удалить</button>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
